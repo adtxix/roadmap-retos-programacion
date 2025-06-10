3b1007e4bcea3b1d51fd7ee0c691d281e126cc3c
adtxix <adtxix@gmail.com>
2025-06-09 20:42:57 -0400

    Update Adtxix.py

--- Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/Adtxix.py
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/Adtxix.py
@@ -1,1 +1,18 @@
+# python.org
+"""
+python.org
+python.org 
+python.org 
+"""
+variable_1 = "cadena de texto"
+constante_1 = "constante_1"
 
+variable_int = 19
+variable_float = 19.5
+variable_bool = verdadero
+variable_list = [1,2,3] variable
+variable_tupla = (5,6,7) inmutable
+variable_dict = {"nombre", "edad", etc}
+
+
+print(Hola, Python)
